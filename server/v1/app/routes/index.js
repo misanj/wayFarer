83e22de15def5f9e35dc26efdad960c21c715c03
temisan otokuefor <temipretty1992@gmail.com>
2019-07-09 00:00:31 +0100

    ch(set-up-server): this sets up wayfarer server [Finishes #167114219]

---
+++ server/v1/app/routes/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+//import userRoutes from './users';
+
+const router = express.Router();
+
+// Home
+router.get('/', (req, res) => res.status(301).redirect('api/v1'));
+router.get('/v1', (req, res) => res.status(200).json({
+  status: res.statusCode,
+  message: 'Welcome to WayFarer API version 1',
+}));
+
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'welcome to version 1 of way farer API' });
+});
+
+// Routes
+
+export default router;
